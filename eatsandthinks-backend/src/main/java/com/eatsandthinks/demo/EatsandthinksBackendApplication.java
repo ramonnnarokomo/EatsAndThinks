0a153ec19ada6e93f9858b1ad40ad4ea6e6fc88b
Ramón Nnar Okomo <ramonnnarokomo@gmail.com>
2025-11-25 17:52:41 +0100

    Debug DB connection

--- eatsandthinks-backend/src/main/java/com/eatsandthinks/demo/EatsandthinksBackendApplication.java
+++ eatsandthinks-backend/src/main/java/com/eatsandthinks/demo/EatsandthinksBackendApplication.java
@@ -2,13 +2,23 @@ package com.eatsandthinks.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-
 public class EatsandthinksBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EatsandthinksBackendApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EatsandthinksBackendApplication.class, args);
+		Environment env = context.getEnvironment();
+		String url = env.getProperty("spring.datasource.url");
+		String user = env.getProperty("spring.datasource.username");
+		
+		System.out.println("=========================================");
+		System.out.println("🔍 DEBUG CONEXIÓN DB");
+		System.out.println("URL: " + url);
+		System.out.println("USER: " + user);
+		System.out.println("=========================================");
 	}
 
 }
